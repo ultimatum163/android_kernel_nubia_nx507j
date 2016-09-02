0d586759a509e5054b2c962746d7b204c7c2c446
ultimatum163 <ultimatum163@yandex.ru>
2016-09-02 12:35:01 +0400

    Rel 3 (2016-09-02)

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 export ARCH=arm
-export CROSS_COMPILE=""
+export CROSS_COMPILE="/home/ultimatum163/cm-12.1-source/prebuilts/gcc/linux-x86/arm/arm-cortex_a15-linux-gnueabihf-linaro_4.9/bin/arm-cortex_a15-linux-gnueabihf-"
 
 
-make nx507j_defconfig
+make nx507j-nubiaui_defconfig
 make -j4
 
 make INSTALL_MOD_STRIP=--strip-unneeded INSTALL_MOD_PATH=out/system INSTALL_MOD_DIR=out/system android_modules_install
